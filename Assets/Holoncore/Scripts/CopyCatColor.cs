fbef7364fc9afc6a9da79bc5061658fde2361cb7
Maf'j Alvarez <1101918+blujay@users.noreply.github.com>
2022-02-09 14:42:03 +0000

    added use-events script

---
+++ Assets/Holoncore/Scripts/CopyCatColor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+public class CopyCatColor : MonoBehaviour
+    {
+        private bool pressedLastFrame = false;
+        private bool clonedLastFrame = false;
+        private GameObject prefabToClone;
+        private string holonName;
+
+    [SerializeField]
+    private Color holonColor;
+
+    public void Awake()
+    {
+        pressedLastFrame = false;
+        clonedLastFrame = false;
+        
+    }
+    public void Start()
+    {
+        prefabToClone = this.gameObject;
+        
+    }
+
+
+    public void Clone()
+    {
+            
+            prefabToClone = this.gameObject;
+            GameObject obj = Instantiate(prefabToClone, prefabToClone.transform.position, prefabToClone.transform.rotation);
+            obj.transform.localScale = prefabToClone.transform.localScale;
+            obj.gameObject.name = holonName + "-" + Time.time;
+            obj.layer = LayerMask.NameToLayer("Cloning");
+            foreach (var go in obj.GetComponentsInChildren<Transform>())
+            {
+                go.gameObject.layer = LayerMask.NameToLayer("Cloning");
+            }
+        }
+    }
---
+++ Assets/Holoncore/Scripts/OnUseEvent.cs
@@ -0,0 +1,59 @@
+using System;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Networking.Pun2
+
+{
+    public class OnUseEvent : MonoBehaviourPun
+
+
+    {
+
+        public UnityEvent OnUseEnter;
+        public UnityEvent OnUseExit;
+
+        private bool pressedLastFrame = false;
+        private bool clonedLastFrame = false;
+        private string myPrefabName;
+        private Transform thisObjectTransform;
+        [NonSerialized] public OVRGrabber grabber;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            float triggerAmount = 0;
+
+            if (photonView.IsMine)
+            {
+
+                triggerAmount = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+
+                if (triggerAmount > 0.75f)
+                {
+                    pressedLastFrame = true;
+
+                    OnUseEnter.Invoke();
+                }
+
+                bool releasedTrigger = triggerAmount < 0.25f && pressedLastFrame;
+
+                if (releasedTrigger)
+                {
+                    pressedLastFrame = false;
+                    clonedLastFrame = false;
+                    OnUseExit.Invoke();
+                    
+
+
+                }
+
+            }
+        }
+    }
+}
