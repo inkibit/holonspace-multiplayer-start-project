3a2844864bf1c94f1505540250f20dd7b90e3702
lucas martinic <42948357+lucas-martinic@users.noreply.github.com>
2022-02-10 17:32:37 +0100

    Recorder spawner

--- Assets/CreateAudioCubeStart.cs
+++ Assets/CreateAudioCubeStart.cs
@@ -5,11 +5,8 @@ public class CreateAudioCubeStart : MonoBehaviour
 {
     [SerializeField] private GameObject audioCube;
 
-    void Start()
+    public void CreateRecorderObject()
     {
-        if (PhotonNetwork.IsMasterClient)
-        {
-            PhotonNetwork.Instantiate(audioCube.name, transform.position, transform.rotation);
-        }
+        PhotonNetwork.Instantiate(audioCube.name, transform.position + new Vector3(Random.Range(-0.2f, 0.2f), Random.Range(0.1f, 0.3f), Random.Range(-0.2f, 0.2f)), transform.rotation);
     }
 }
--- Assets/Holoncore/Scripts/Triggered.cs
+++ Assets/Holoncore/Scripts/Triggered.cs
@@ -4,10 +4,27 @@ using UnityEngine.Events;
 public class Triggered : MonoBehaviour
 {
     public UnityEvent TriggerEvent;
+    [SerializeField] float cooldown = 0;
+    private float counter;
+    private bool onCooldown = false;
     
     private void OnTriggerEnter(Collider other)
     {
+        if (onCooldown) return;
         TriggerEvent.Invoke();
+        onCooldown = true;
     }
-    
+
+    private void Update()
+    {
+        if (onCooldown)
+        {
+            counter += Time.deltaTime;
+            if(counter >= cooldown)
+            {
+                onCooldown = false;
+                counter = 0;
+            }
+        }
+    }
 }
