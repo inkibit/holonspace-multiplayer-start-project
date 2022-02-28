34a2739bc5f3cb740498a781bc401865b5890149
Maf'j Alvarez <1101918+blujay@users.noreply.github.com>
2022-02-28 15:30:07 +0000

    changing scripting backend and fixing issues with camera jitter

---
+++ Assets/fogControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fogControl : MonoBehaviour
+{
+
+    public Color fogColorSet;
+    public float fogDensitySet;
+
+    private void Update()
+    {
+        RenderSettings.fogColor = fogColorSet;
+        RenderSettings.fogDensity = fogDensitySet;
+
+    }
+}
